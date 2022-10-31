ddbf049bb1df5d521d992d2c9aff3ba5d97ffe90
aline.silva <aline.silva@valtech.com>
2022-10-31 13:45:27 -0300

    meu primeiro commit

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -54,7 +54,7 @@ describe('Signup', ()=> {
             {field: 'email', output: 'É necessário informar o email'},
             {field: 'postalcode', output: 'É necessário informar o CEP'},
             {field: 'number', output: 'É necessário informar o número do endereço'},
-            {field: 'delivery_method', output: 'Selecione o método de entrega'},
+            {field: 'delivery_method', output: 'Selecione o método de entreg'},
             {field: 'cnh', output: 'Adicione uma foto da sua CNH'},
         ]
 
